b452b569af55c2c4b55f30b2160e2fdf26cc256c
venderjin <dydgh3331@naver.com>
2025-03-05 09:34:36 +0900

    set dev environment - tailwind, prettier, fonts

---
+++ src/app/(pages)/grid-stack/page.tsx
@@ -0,0 +1,13 @@
+export default function GridStackPage() {
+    return (
+        <>
+            <div className="bg-gray-400 text-3xl font-bold">
+                grid stack test page
+                <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+                    Submit
+                </button>
+                <div className="bg-regal-blue"> test custom</div>
+            </div>
+        </>
+    )
+}
